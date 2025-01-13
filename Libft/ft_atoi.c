1ec8a1c0bec0b7efd962148ab370d302b0942d81
s-bolivar <bolivarss1212@gmail.com>
2025-01-13 17:54:34 +0100

    Add files via upload

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbolivar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/28 13:39:08 by sbolivar          #+#    #+#             */
+/*   Updated: 2024/09/28 13:39:10 by sbolivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	res = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+	{
+		sign *= -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while ('0' <= *str && *str <= '9')
+	{
+		res = (res * 10) + (*str - '0');
+		str++;
+	}
+	return (res * sign);
+}
+
+/*int main()
+{
+	const char *str;
+	str = "-2147483649";
+	printf("%d\n", ft_atoi(str));
+}*/
